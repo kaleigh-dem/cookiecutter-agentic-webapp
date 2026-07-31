ba7064f14891a32b58c5b076196a44937210e7c7
kaleigh-dem <kaleigh.demartino@gmail.com>
2026-07-31 11:24:21 -0400

    Allow configured web origin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.WEB_ORIGIN ?? 'http://localhost:3000',
+  });
   app.enableShutdownHooks();
   app.setGlobalPrefix('api');
 
