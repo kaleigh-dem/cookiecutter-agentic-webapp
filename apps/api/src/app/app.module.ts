65b1b58bbf57c45d147939ec238004a7e03b3085
kaleigh-dem <kaleigh.demartino@gmail.com>
2026-07-31 16:48:41 -0400

    feat(api): register observability module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AgentTasksModule } from './agent-tasks/agent-tasks.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ObservabilityModule } from './observability/observability.module';
 
 @Module({
-  imports: [AgentTasksModule],
+  imports: [AgentTasksModule, ObservabilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
