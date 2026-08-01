a6b706c8fae5f1778a945a348fceab6f57e6c770
kaleigh-dem <kaleigh.demartino@gmail.com>
2026-08-01 17:46:42 -0400

    keep release version source focused

--- tools/workspace-plugin/src/template-version.ts
+++ tools/workspace-plugin/src/template-version.ts
@@ -1,6 +1,1 @@
-export const upstreamTemplateRepository = [
-  ['kaleigh', 'dem'].join('-'),
-  ['cookiecutter', ['agentic', 'webapp'].join('-')].join('-'),
-].join('/');
-
 export const templateVersion = '0.1.0';
