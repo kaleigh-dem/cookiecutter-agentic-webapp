c7fc6427efe17eb7c5f4459ee7a7ab049c3a35c7
kaleigh-dem <kaleigh.demartino@gmail.com>
2026-08-02 07:27:40 -0400

    fix(env): remove unused Redis requirement

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -7,7 +7,6 @@ const schema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
-  REDIS_URL: z.string().url(),
 });
 
 export type ServerEnvironment = z.infer<typeof schema>;
